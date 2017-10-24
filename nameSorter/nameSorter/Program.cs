8636d4b99b10f8b364622f780484ba5098308d77
Hakubo72, Mind Games Entertainment <Hakubo72@gmail.com>
2017-10-24 16:33:51 +1000

    Base solution added & repo set up

---
+++ nameSorter/nameSorter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nameSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press Any Key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
