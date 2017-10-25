860b5c50d71d5d877eea3c8a7cb8ecdc08a3fd55
Haku72 <33048275+Haku72@users.noreply.github.com>
2017-10-25 20:08:59 +1000

    Dev (#2)
    * Base solution added & repo set up
    
    * NEW file names.txt
    
    read from text file using streamreader, generate string array containing given/surname, add to list, sort by name order, join array into string and write to console
    
    * Moved functionality into separate class 'nameSorter'
    Program modified to call class methods and write to console
    UnitTests modified

---
+++ nameSorter/application/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new NameSorter();
+            Console.WriteLine("nameSorter initialised.\n\n");
+
+            string input= null;
+
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                while(input == null)
+                {
+                    Console.WriteLine("Please enter correct file path:\n");
+                    input = Console.ReadLine();
+
+                    if (input != null && (File.Exists(Path.GetFullPath(input))))
+                    {
+                        break;
+                    }
+                    input = null;
+                }
+            }
+            else
+            {
+                input = args[0];
+            }
+
+            app.parseFile(input);
+            Console.Write("Reading file...\n");
+
+            app.sort();
+            Console.Write("Sorting...\n");
+
+            app.writeToFile();
+            Console.Write("Writing to file...\n\n");
+
+            app.displaySorted();
+            Console.Write("\n\nSort Complete. Press Any Key to exit...");
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ nameSorter/application/nameSorter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class NameSorter
+{
+    public string filePath { get; private set; }
+    public List<String[]> namesList_unsorted { get; private set; }
+    public List<String[]> namesList_sorted { get; private set; }
+
+
+    public NameSorter()
+    {
+        filePath = null;
+        namesList_unsorted = new List<string[]>();
+        namesList_sorted = new List<string[]>();
+    }
+
+    public void parseFile(String path)
+    {
+        filePath = Path.GetDirectoryName( Path.GetFullPath(path) );
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                namesList_unsorted.Add(line.Split());
+            }
+        }
+
+        namesList_sorted.Capacity = namesList_unsorted.Count;
+    }
+
+    public void sort()
+    {
+        namesList_sorted = namesList_unsorted.OrderBy(name => name.Last()).
+            ThenBy(name => name.First()).
+            ThenBy(name => name.ElementAtOrDefault(2)).
+            ThenBy(name => name.ElementAtOrDefault(3)).
+        ToList();
+    }
+
+    public void writeToFile()
+    {
+        if(filePath == null)
+        {
+            throw new ArgumentNullException(filePath, "filePath not set");
+        }
+
+        if (namesList_sorted.Count == 0)
+        {
+            throw new NullReferenceException("namesList_sorted empty");
+        }
+
+        string path = filePath + @"\sorted-names-list.txt";
+
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            foreach (String[] p in namesList_sorted)
+            {
+                string s = String.Join(" ", p);
+                writer.WriteLine(s);
+            }
+        }
+    }
+
+    public void displaySorted()
+    {
+        foreach (String[] p in namesList_sorted)
+        {
+            string s = String.Join(" ", p);
+            Console.WriteLine(s);
+        }
+    }
+}
