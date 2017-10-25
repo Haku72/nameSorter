b62c3800226f86df6f6d3fc0156f5118f63267c0
Hakubo72, Mind Games Entertainment <Hakubo72@gmail.com>
2017-10-25 10:33:34 +1000

    NEW file names.txt
    read from text file using streamreader, generate string array containing given/surname, add to list, sort by name order, join array into string and write to console

--- nameSorter/nameSorter/Program.cs
+++ nameSorter/nameSorter/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace nameSorter
 {
+    class Person
+    {
+        public string[] name;
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Press Any Key to continue...");
+            String path = "..\\names.txt";
+
+            List<Person> list = new List<Person>();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ( (line = reader.ReadLine()) != null)
+                {
+                    Person person = new Person();
+                   person.name = line.Split();
+                    list.Add(person);
+                }
+            }
+
+            var sortedList = list.OrderBy(p => p.name.Last()).
+                ThenBy(p => p.name.First());
+                //ThenBy(p => p.name.Last()).
+                //ThenBy(p => p.name.Last());
+
+            foreach(Person p in sortedList)
+            {
+                string s = String.Join(" ", p.name);
+                Console.WriteLine(s);
+            }
+
+            Console.Write("\nPress Any Key to continue...");
             Console.ReadKey(true);
         }
     }
